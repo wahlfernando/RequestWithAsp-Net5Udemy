5d0388cbb305b60ef80a87b38f1779dccf74d60c
Fernando Alexandre Wahl <wahlfernando@yahoo.com.br>
2021-10-09 11:18:18 -0300

    inclido a iniciação dos 4 verbos

---
+++ 02_RestWithAsp-Net5Udemy_Calculadora/RestWithAsp-Net5Udemy/RestWithAsp-Net5Udemy/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAsp_Net5Udemy.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public long FirtName { get; set; }
+        public long LasName { get; set; }
+        public long Adress { get; set; }
+        public long Gender { get; set; }
+    }
+}
